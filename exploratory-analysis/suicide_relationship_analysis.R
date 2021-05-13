677f94fae254d60880c5c324bb4264ab7f665c2e
Rebecca Klein <rebecca.anne.klein@gmail.com>
2021-05-12 18:13:27 -0700

    finish suicide relationship analysis

--- exploratory-analysis/suicide_relationship_analysis.R
+++ exploratory-analysis/suicide_relationship_analysis.R
@@ -1,12 +1,16 @@
 # Load libraries
 library(dplyr)
 library(skimr)
+library(plotly)
+library(reshape)
 
 # Load data
-suicide_data <- read.csv("../data/national/Age-standardized suicide rates.csv", check.names=F)
-crude_suicide_data <- read.csv("../data/national/Crude suicide rates.csv", check.names=F)
+suicide_data <- read.csv("../data/national/Age-standardized suicide rates.csv",
+                         check.names = F)
+crude_suicide_data <- read.csv("../data/national/Crude suicide rates.csv",
+                               check.names = F)
 facilities_data <- read.csv("../data/national/Facilities.csv")
-HR_data <- read.csv("../data/national/Human Resources.csv", check.names=F)
+HR_data <- read.csv("../data/national/Human Resources.csv", check.names = F)
 
 # Analyze data
 skim(suicide_data)
@@ -17,13 +21,13 @@ skim(HR_data)
 # Notes
 
 # suicide_data:
-# age-standardized data on suicide rates per 
+# age-standardized data on suicide rates per
  # -country (183)
  # -sex (male, female, both)
  # -year (2000, 2010, 2015, 2016)
 # skewed right
 # mean over the years is increasing
-# one row has 0 value (Antigua and Barbuda), 
+# one row has 0 value (Antigua and Barbuda),
   # probably because of rounding/smaller population
 # no missing values
 
@@ -49,7 +53,7 @@ skim(HR_data)
 # one row has 0 value (Tonga), probably because of rounding/smaller population
 
 # HR_data:
-# number of human resources working in mental health sector 
+# number of human resources working in mental health sector
   #per 100,000 population per country (107)
 # -Psychiatrists (3 missing, 97.2% complete)
 # -Nurses (16 missing, 85.0% complete)
@@ -57,6 +61,94 @@ skim(HR_data)
 # -Psychologists (23 missing, 78.5% complete, may not include)
 # heavily skewed right
 # two rows has 0 value
- # -Guyana: psychologists, probably because of rounding 
+ # -Guyana: psychologists, probably because of rounding
             # (currently only have 27 psychologists and psychiatrists)
  # -Columbia: nurses, reason is unknown - possibly accurate
+
+# Process data for graphics
+
+suicide_data_2016 <- suicide_data[c("Country", "Sex", "2016")]
+suicide_data_2016 <- dplyr::rename(suicide_data_2016, suicide_rate = "2016")
+joined_suicide_data <- merge(suicide_data_2016,
+                             subset(facilities_data, select = -c(Year)),
+                             by = "Country")
+joined_suicide_data <- merge(joined_suicide_data,
+                             subset(HR_data, select = -c(Year)),
+                             by = "Country")
+
+long_crude_suicide_data <- melt(crude_suicide_data, id = c("Country", "Sex"))
+
+joined_crude_suicide_data <- merge(long_crude_suicide_data,
+                                   subset(facilities_data, select = -c(Year)),
+                                   by = "Country")
+joined_crude_suicide_data <- merge(joined_crude_suicide_data,
+                                   subset(HR_data, select = -c(Year)),
+                                   by = "Country")
+
+# Create plot
+
+suicide_relationship_plot <- plot_ly(
+    data = joined_suicide_data,
+    type = "scatter",
+    x = ~suicide_rate,
+    y = ~Psychiatrists,
+    mode = "markers",
+    text = paste("Suicide Rate :", joined_suicide_data$suicide_rate,
+                 "<br>Country :", joined_suicide_data$Country),
+    hoverinfo = "text",
+    transforms = list(
+      list(
+        type = "filter",
+        target = ~Sex,
+        operation = "=",
+        value = unique(joined_suicide_data$Sex)[1]
+      ))) %>%
+  layout(
+    title = "Suicide Rates vs Facility and Human Resources",
+    xaxis = list(title = "Suicide Rate"),
+    yaxis = list(title = "Psychiatrists per 100,000 pop"),
+    updatemenus = list(
+      list(
+        y = 1,
+        buttons = list(
+          list(method = "update",
+            args = list(list(y = list(joined_suicide_data$Psychiatrists)),
+                  list(yaxis = list(title = "Psychiatrists per 100,000 pop"))),
+            label = "Psychiatrists"),
+          list(method = "update",
+               args = list(list(y = list(joined_suicide_data$Nurses)),
+                      list(yaxis = list(title =
+                                  "Mental Health Nurses per 100,000 pop"))),
+               label = "Nurses"),
+          list(method = "update",
+            args = list(list(y = list(joined_suicide_data$Mental._hospitals)),
+                list(yaxis = list(title = "Mental Hospitals per 100,000 pop"))),
+            label = "Mental Hospitals"),
+          list(method = "update",
+            args = list(list(y = list(joined_suicide_data$health_units)),
+                    list(yaxis = list(title = "Health Units per 100,000 pop"))),
+            label = "Health Units"),
+          list(method = "update",
+            args = list(list(y =
+                        list(joined_suicide_data$outpatient._facilities)),
+                  list(yaxis = list(title =
+                              "Outpatient Facilities per 100,000 pop"))),
+            label = "Outpatient Facilities"))),
+      list(
+        y = .9, # depending on the size of the plot, this should be changed
+        buttons = list(
+          list(method = "restyle",
+               args = list("transforms[0].value",
+                           unique(joined_suicide_data$Sex)[1]),
+               label = unique(joined_suicide_data$Sex)[1]),
+          list(method = "restyle",
+               args = list("transforms[0].value",
+                           unique(joined_suicide_data$Sex)[2]),
+               label = unique(joined_suicide_data$Sex)[2]),
+          list(method = "restyle",
+               args = list("transforms[0].value",
+                           unique(joined_suicide_data$Sex)[3]),
+               label = unique(joined_suicide_data$Sex)[3])
+        )
+      )
+    ))
