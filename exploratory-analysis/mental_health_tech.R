058cd7c029e0edff804aec985a44f539b4429cc5
Damita Gomez <damita.gomez@comcast.net>
2021-05-12 23:04:56 -0700

    Update mental health in tech page

--- exploratory-analysis/mental_health_tech.R
+++ exploratory-analysis/mental_health_tech.R
@@ -6,7 +6,7 @@ library("tidyr")
 data <- read.csv("../data/work-life/mental-heath-in-tech-2014.csv")
 
 data <- data %>%
-  drop_na(treatment, seek_help)
+  drop_na(work_interfere, seek_help)
 
 heatmap_data <- data %>%
   select(work_interfere, seek_help)
@@ -29,4 +29,10 @@ work_help_heatmap <- ggplot(data = treatment_df) +
   scale_fill_gradient2("number of employees", low = "white",
                        mid = "light blue", high = "dark blue")
 
+sometimes_count <- nrow(heatmap_data[heatmap_data$work_interfere ==
+                                       "Sometimes",])
+
+seek_help_count <- nrow(heatmap_data[heatmap_data$seek_help == "No",])
+
+total_respondents <- nrow(heatmap_data)
 
